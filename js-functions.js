db734623f1a8989bd41c2d335726bd4224ec5bb5
Rthr-X <rthr_x@yahoo.com>
2019-08-30 09:18:16 -0400

    Update js-functions.js

--- js-functions.js
+++ js-functions.js
@@ -112,6 +112,166 @@ Array.prototype.replace = function (object, uid) {
     }
     return this;
 };
+
+/**
+ * Filter an array by elements which match the .contains(query) check; notably, this will only
+ * return string values (or any other type that has a 'contains' method)
+ *
+ * @example ['one', 'two', 'three'].containsString('e') === ['one', 'three']
+ *
+ * @param {string} query                  A string to search this array for
+ * @param {bool} caseSensitive (optional) If true, enforce case sensitivity (default false)
+ * @return {array}                        Array of entries that match the query
+ */
+Array.prototype.containsString = function (query, caseSensitive) {
+    caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
+    
+    var itemsFound = [];
+    this.forEach(function (item) {
+        if (typeof item.contains !== 'undefined' && item.contains(query, caseSensitive)) {
+            itemsFound.push(item);
+        }
+    });
+    return itemsFound;
+};
+
+/**
+ * Find the lowest value in the array
+ *
+ * @return {number}
+ */
+Array.prototype.min = function () {
+    return Math.min.apply(Math, this);
+};
+
+/**
+ * Find the highest value in the array
+ *
+ * @return {number}
+ */
+Array.prototype.max = function () {
+    return Math.max.apply(Math, this);
+};
+
+/**
+ * Return the last element in an array or null if nothing
+ *
+ * @example [1, 2, 3].end() === 3
+ *
+ * @return {varies}
+ */
+Array.prototype.end = function () {
+    return this.length ? this[this.length - 1] : null;
+};
+ 
+/**
+ * Stand-alone version of the above
+ *
+ * @example Array.end([1, 2, 3]) === 3
+ *
+ * @param {array} input Array
+ * @return {varies}
+ */
+Array.end = function (input) {
+    return (input && typeof input === 'object' && input.constructor === Array && input.length) ? input[input.length - 1] : null;
+};
+
+/* ==================================================================================================== */
+* NUMBER */
+
+/**
+ * Mostly used as a safety net; this allows for a variable which may or may not be a number to 
+ * call 'variable.toInt()', and both String and Number values will return an integer value
+ *
+ * @return {int}
+ */
+Number.prototype.toInt = function () {
+    return parseInt(this);
+};
+
+/* ==================================================================================================== */
+/* STRING */
+
+/**
+ * Check if a string contains the query string within it anywhere; by default, this check
+ * is NOT CASE SENSITIVE; set the caseSensitive argument to true to force that behavior
+ *
+ * @param {string} query       A string to check for
+ * @param {bool} caseSensitive Force the check to be case-sensitive (default false)
+ * @return {bool}              Returns true if the query was found in the string
+ */
+ String.prototype.contains = function (query, caseSensitive) {
+     caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
+     return (
+         caseSensitive
+         ? this.indexOf(query) !== -1
+         : this.toLowerCase().indexOf(query.toLowerCase()) !== -1
+     );
+ };
+
+/**
+ * Get the integer value of a string, or 0 if the string is not a numeric value; Note that
+ * a string like '3xx' will return a value of 3
+ *
+ * @return {int} The integer value of this string; returns a 0 instead of NaN
+ */
+String.prototype.toInt = function () {
+    return parseInt(this) ? parseInt(this) : 0;
+};
+
+/**
+ * Check if a string is actually a JSON object (that is, it can be parsed as a valid JSON object);
+ * Note that the strings 'true' and 'false' will return true, as those ARE valid JSON values
+ *
+ * @return {bool}
+ */
+String.prototype.isJSON = function () {
+    var isValidJSON = true;
+    try {
+        JSON.parse(this);
+    } catch (error) {
+        isValidJSON = false;
+    }
+    return isValidJSON;
+};
+
+/**
+ * A JavaScript implementation of the PHP printf() function
+ *
+ * @example 'This is a {0} string {1}'.format('formatted', 'dude') === 'This is a formatted string dude'
+ *
+ * @return {string}
+ */
+String.prototype.format = function () {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
+ 
+/* ==================================================================================================== */
+/* PARSE */
+
+/**
+ * Parse is a "static class", with several static methods for interpreting various input
+ * values in a consistent manner
+ */
+function Parse() {'use strict';}
+
+/**
+ * Retrieve the integer value of the input; utilizes the 'toInt' method of the input if
+ * it exists (usually for String and Number; add a .toInt method to custom objects to
+ * use that as well)
+ *
+ * @param {varies}
+ * @return {int}
+ */
+Parse.int = function (input) {
+    return typeof input.toInt !== 'undefined' ? input.toInt() : parseInt(input);
+};
+
  
  
  
